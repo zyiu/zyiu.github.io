4c9b65190699f8f7c2fa12689337a8593cdd0321
Zachary Yiu <zyiu@me.com>
2016-04-25 00:59:59 -0700

    added descriptions to subarticles with buttons and animations through script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+$('.sub-article-button').click(function() {
+    var height = parseInt($(this).parent().css('height').replace("px", ""))
+    if (height == 250) {
+        $(this).parent().css('height', 50);
+        $(this).children().attr('src','assets/img/arrow-up.png');
+    } else {
+        $(this).parent().css('height', 250);
+        $(this).children().attr('src','assets/img/arrow-down.png');
+    }
 
+});
 
 });
