0df7b397198627793fae3dfcc19a00d3e31afe94
Zachary Yiu <zyiu@me.com>
2016-04-28 14:54:13 -0700

    changed sub-article titles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 $('.sub-article-button').click(function() {
-    var height = parseInt($(this).parent().css('height').replace("px", ""))
+    var height = parseInt($(this).parent().css('height').replace("px", ""));
     if (height == 250) {
         $(this).parent().css('height', 50);
         $(this).children().attr('src','assets/img/arrow-up.png');
@@ -15,7 +15,7 @@ $('.title').hover(function() {
     $('.title').css('border-bottom', '5px solid #ff0066');
 },  function() {
     $('.title').css('border-bottom', '5px solid #5bf26e');
-})
+});
 
 $("#card").flip();
 
